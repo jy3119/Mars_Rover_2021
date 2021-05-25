99a70331870aa4b2292aada07755a5440a7f8267
tsersiew <tys19@ic.ac.uk>
2021-05-25 13:03:35 +0800

    started working on destination output

--- frontend/src/components/DestinationOutput/DestinationOutput.js
+++ frontend/src/components/DestinationOutput/DestinationOutput.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import { useDispatch, useSelector} from 'react-redux';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core/';
+import DeleteIcon from '@material-ui/icons/Delete';
+import moment from 'moment';
+import { useDispatch } from 'react-redux';
+
 import useStyles from './styles';
 
-const DestinationOutput = () => {
-    //const dispatch = useDispatch();
-    const destinations = useSelector((state)=>state.destinations);
-    const classes = useStyles();
-
-    console.log(destinations);
+const DestinationOutput = (destination) => {
+  //const destinations = useSelector((state)=>state.destinations);
+  const dispatch = useDispatch();
+  const classes = useStyles();
   
-    return (
-      <h1>DestinationOutput</h1>
-    );
-  };
+  return (
+    <Card className={classes.card}>
+      <div className={classes.overlay}>
+        <Typography variant="body2">{moment(destination.SetAt).fromNow()}</Typography>
+      </div>
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p" align="center">{destination.x_coordinate}</Typography>
+        <Typography variant="body2" color="textSecondary" component="p" align="center">{destination.y_coordinate}</Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <Button size="small" color="primary" onClick={() => dispatch(deleteDestination(destination._id))}><DeleteIcon fontSize="small" /> Delete</Button>
+      </CardActions>
+    </Card>
+  );
+};
+  
   
   export default DestinationOutput;
   
