5a1f1afeca9c878bb04d7ca8b1afd83b869b9d07
tsersiew <tys19@ic.ac.uk>
2021-05-28 18:41:35 +0800

    set up mqtt-broker on aws iot

--- mqtt-broker/broker.js
+++ mqtt-broker/broker.js
@@ -1,11 +1,20 @@
+//https://www.compose.com/articles/building-mongodb-into-your-internet-of-things-a-tutorial/ 
+
 /********* Set up MQTT Broker *********/
 var mqtt = require('mqtt'); //includes mqtt server 
+//MONGODB
 var mongodb = require('mongodb'); // includes mongoDB 
 var mongodbClient = mongodb.MongoClient; //initialises the mongoDB client
 var mongodbURI='mongodb://username:password@server.mongohq.com:port/database';
 var deviceRoot = "demo/device/"; //deviceroot is topic name given in arduino code 
 var collection,client; //initialise collection and client
 
+//AWS
+var client = mqtt.connect("ws://18.188.43.23:9001", {
+  //open connection with your broker in AWS via websocket
+  username:"mqtt-broker", //authenticate your broker with username and password
+  password:"coolbeans$4",
+}); 
 
 /********* Initialise Broker **********/
 mongodbClient.connect(mongodbURI, setupCollection); //connect the database with collecion
@@ -41,3 +50,23 @@ function insertEvent(topic,message) {
 		}
 	);
 }
+
+//ESP32 part
+sensor.read(11, 4, function (err, temperature, humidity) {
+    //read sensor data from raspberry pi pin GPIO 4
+    function timer(temp, humid) {
+        client.publish(
+          "mqtt/dht",
+          JSON.stringify(`Temperature:${temp}C & Humidity:${humid}%`) //convert number to string
+        ); //publish sensor data to broker on topic mqtt/dht
+  
+        console.log("topic published to the broker");
+     
+    }
+  
+    setInterval(timer, 5000, temperature, humidity); // send sensor data to broker every 5 seconds
+  });
+
+
+
+//AWS Public IPv4 Address: 18.188.43.23
