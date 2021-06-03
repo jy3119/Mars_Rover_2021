e6b836eca29a97337a1cac63acb90f77f9b6d598
ml3319 <56972053+ml3319@users.noreply.github.com>
2021-06-03 18:34:45 +0800

    edited i2c link codes for esp32 and also fpga c file to account for angle instead of hori

--- control/vision_fpga/i2c_fpga_test.c
+++ control/vision_fpga/i2c_fpga_test.c
@@ -1,28 +1,37 @@
+/* NOTES:
+   each digit of hex number represents 4 bits in binary so alt_16 will be a 4 digit hex number, alt_8 will be a 2 digit hex number
+   to convert to signed 2's complement, invert all bits of the binary number, and then add 1 after inversion.
+   so e.g:
+      45 degrees = 0x2D i.e. 00101101
+     -45 degrees = 0xD3 i.e. 11010011
+*/
+
 #include "sys/alt_stdio.h"
 #include "system.h"
 #include "io.h"
 
 #define mem_limit 1024
+alt_16 diag_dist;			 // diagonal distance of obstacle to rover
+alt_8 angle;                // angle of obstacle from rover. to left of rover: +ve, to right of rover: -ve
+alt_8 detected;    // 1 only if obstacle has been detected, 0 otherwise
 
-void updateTmp(alt_32 base_addr, alt_8 diag_dist, alt_8 hori_dist, alt_8 leftctr_or_right, alt_8 obstacle_detected){
-	alt_32 tmp = (diag_dist<<24) + (hori_dist<<16) + (leftctr_or_right<<8) + obstacle_detected;
+void updateTmp(alt_32 base_addr, alt_16 diag_dist, alt_8 angle, alt_8 detected){
+	alt_32 tmp = (diag_dist<<16) + (angle<<8) + detected;
 	IOWR(base_addr, 0, tmp);
 }
-
 int main()
 { 
   alt_putstr("Hello from Nios II!\n");
 
   // EDIT VALUES OF PARAMETERS HERE as rover moves around.
-  alt_8 diag_dist = 0x05;			      // diagonal distance of obstacle to rover, up to 255cm or 0xff
-  alt_8 hori_dist = 0x04;		        // horizontal distance of obstacle to rover, up to 255cm or 0xff
-  alt_8 leftctr_or_right = 0x00;    // 1 if obstacle is to the right of rover, 0 otherwise (left or ctr of rover)
-  alt_8 obstacle_detected = 0x01;   // 1 only if obstacle has been detected, 0 otherwise
+  diag_dist = 0x0503;			       // diagonal distance of obstacle to rover in cm
+  angle = 0xa6;                  // angle of obstacle from rover. to left of rover: +ve, to right of rover: -ve
+  detected = 0x01;      // 1 only if obstacle has been detected, 0 otherwise
 
   alt_putstr("Writing into memory...\n");
 
   // writes into the 1st word of I2C MEM
-  updateTmp(I2C_MEM_BASE, diag_dist, hori_dist, leftctr_or_right, obstacle_detected);
+  updateTmp(I2C_MEM_BASE, diag_dist, angle, detected);
 
   alt_putstr("Memory write done!\n");
 
